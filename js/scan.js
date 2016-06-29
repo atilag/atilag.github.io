ed661cdaf0e60b125339840d463e3d546692ea52
Juan Gomez <atilag@gmail.com>
2016-06-29 15:11:36 +0200

    Removing directory links

---
+++ js/scan.js
@@ -0,0 +1,12 @@
+$('#scanButton').on('click', function (e) {
+	navigator.bluetooth.requestDevice({
+		filters: [{
+			services: [0xFEAA]
+		}]
+	});
+
+}).then( device => {
+
+}).catch( error => { 
+	console.error(); 
+});
